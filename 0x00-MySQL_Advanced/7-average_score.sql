a663fa2567998072c69ecdf48107ed964ac5ae39
adiomi90 <adiomi90@outlook.com>
2024-06-13 22:02:17 +0300

    updates

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,20 +1,19 @@
--- Stored procedure to compute and store average score for a user
+-- Stored procedure to compute and
+-- store average score for a user
+DROP procedure IF EXISTS ComputeAverageScoreForUser;
 DROP procedure IF EXISTS ComputeAverageScoreForUser;
 DELIMITER //
 
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(
+	IN user_id INT
+)
 BEGIN
-    DECLARE avg_score FLOAT;
+	UPDATE users
+   	SET average_score=(SELECT AVG(score) FROM corrections
+			     WHERE corrections.user_id=user_id)
+	WHERE id=user_id;
 
-    -- Compute the average score for the user
-    SELECT AVG(score) INTO avg_score
-    FROM corrections
-    WHERE user_id = user_id;
-
-    -- Update the average score in the users table
-    UPDATE users
-    SET average_score = avg_score
-    WHERE id = user_id;
-END //
+END;
+//
 
 DELIMITER ;
