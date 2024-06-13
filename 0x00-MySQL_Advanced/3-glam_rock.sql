000e45f583b3f421e9e06f9861d711e6cf08aa8b
adiomi90 <adiomi90@outlook.com>
2024-06-13 21:58:07 +0300

    update on errors

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,15 +1,7 @@
 -- This would be done through the import process, assuming the provided file metal_bands.sql.zip
 -- For the purpose of this script, we assume the data is already imported
 
-SELECT
-    name AS band_name,
-    CASE
-        WHEN split IS NULL THEN 2022 - formed
-        ELSE split - formed
-    END AS lifespan
-FROM
-    metal_bands
-WHERE
-    style LIKE '%Glam rock%'
-ORDER BY
-    lifespan DESC;
+SELECT band_name,
+          (COALESCE(split, 2022) - formed) AS lifespan
+	FROM metal_bands
+	WHERE style like '%Glam rock%';
