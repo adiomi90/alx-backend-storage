6fa20df2740b96d9685ab2f564c86fb6e32da1a7
adiomi90 <adiomi90@outlook.com>
2024-06-19 05:05:23 +0300

    log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,24 +2,30 @@
 """ a function that provides some stats about Nginx logs stored in MongoDB """
 from pymongo import MongoClient
 
-# Connect to the MongoDB server running on localhost at port 27017
-client = MongoClient('mongodb://localhost:27017/')  # Adjust the connection string as needed
-db = client.logs
-collection = db.nginx
 
-# Count the total number of documents in the collection
-total_logs = collection.count_documents({})
-
-# Count the number of documents for each HTTP method
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-method_counts = {method: collection.count_documents({"method": method}) for method in methods}
-
-# Count the number of documents with method=GET and path=/status
-get_status_count = collection.count_documents({"method": "GET", "path": "/status"})
-
-# Display the results
-print(f"{total_logs} logs")
-print("Methods:")
-for method in methods:
-    print(f"\tmethod {method}: {method_counts[method]}")
-print(f"{get_status_count} status check")
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.27017/')
+    collection = client.logs.nginx
+    print(f"{col.estimated_document_count()} logs")
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        count = col.count_documents({'method': method})
+        print("\tmethod {}: {}".format(method, count))
+    status_get = col.count_documents({'method': 'GET', 'path': "/status"})
+    print("{} status check".format(status_get))
+    print("IPs:")
+    topIps = col.aggregate([
+        {"$group": {
+            "_id": "$ip",
+            "count": {"$sum": 1}
+        }},
+        {"$sort": {"count": -1}},
+        {"$limit": 10},
+        {"$project": {
+            "_id": 0,
+            "ip": "$_id",
+            "count": 1
+        }}
+    ])
+    for ip in topIps:
+        print("\t{}: {}".format(ip.get('ip'), ip.get('count')))
