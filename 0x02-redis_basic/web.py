e3ba6a9db686a2c6927134dd2d3265ecd00d076c
adiomi90 <adiomi90@outlook.com>
2024-06-20 21:52:01 +0300

    pycodestyle

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,46 +1,39 @@
 #!/usr/bin/env python3
 """Module to fetch and cache a webpage's HTML content using Redis."""
-
-import requests
 import redis
-from typing import Optional
+import requests
+from functools import wraps
 
-# Initialize Redis client globally
 r = redis.Redis()
 
 
+def url_access_count(method):
+    """decorator for get_page function"""
+    @wraps(method)
+    def wrapper(url):
+        """wrapper function"""
+        key = "cached:" + url
+        cached_value = r.get(key)
+        if cached_value:
+            return cached_value.decode("utf-8")
+
+            # Get new content and update cache
+        key_count = "count:" + url
+        html_content = method(url)
+
+        r.incr(key_count)
+        r.set(key, html_content, ex=10)
+        r.expire(key, 10)
+        return html_content
+    return wrapper
+
+
+@url_access_count
 def get_page(url: str) -> str:
-    """Fetch the HTML content of a URL and cache it.
-
-    Args:
-        url (str): The URL to fetch.
-
-    Returns:
-        str: The HTML content of the URL.
-    """
-    count_key = f"count:{url}"
-    cache_key = f"cached:{url}"
-
-    # Increment the access count for the URL
-    r.incr(count_key)
-
-    # Check if the content is already cached
-    cached_content: Optional[bytes] = r.get(cache_key)
-    if cached_content:
-        return cached_content.decode("utf-8")
-
-    # Fetch the content from the URL
-    response = requests.get(url)
-    html_content = response.text
-
-    # Cache the content with an expiration time of 10 seconds
-    r.set(cache_key, html_content, ex=10)
-
-    return html_content
+    """obtain the HTML content of a particular"""
+    results = requests.get(url)
+    return results.text
 
 
 if __name__ == "__main__":
-    url = "http://slowwly.robertomurray.co.uk/delay\
-            /5000/url/http://www.example.com"
-    print(get_page(url))
-    print(get_page(url))
+    get_page('http://slowwly.robertomurray.co.uk')
